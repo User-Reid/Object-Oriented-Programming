207886577157d370294df6c54478e662d1e1ab32
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-14 10:36:37 -0500

    completed assignment involving classes, optional paramaters, constructor overloading, access modifiers and more! first try btw!

--- Program.cs
+++ Program.cs
@@ -1,55 +1,102 @@
-﻿var medicalAppointment = new MedicalAppointment("Brandon", 05/30/2923);
-medicalAppointment.Reschedule(new DateTime(2023, 4, 4));
+﻿// var medicalAppointment = new MedicalAppointment("Brandon", 05 / 30 / 2923);
+// var appointmentOneWeekFromNow = new MedicalAppointment("Margerett Smith");
+// medicalAppointment.Reschedule(new DateTime(2023, 4, 4));
+
+// Dog.Describe("Lucky", "german shepherd", 40);
+var taco = new Dog("Lucky", "german shepherd", 40);
+var banana = new Dog("Salad Dressing", "poodle", 2);
+var apple = new Dog("Butthead", "dragon", 20);
+taco.Describe();
+apple.Describe();
+banana.Describe();
 
 Console.ReadKey();
 
-class MedicalAppointmentPrinter
+class Dog
 {
-  public void Print(MedicalAppointment medicalAppointment)
-  {
-    System.Console.WriteLine("Appointment will take place on " + medicalAppointment.GetDate());
-  }
-}
+  string _name;
+  string _breed;
+  int _weight;
 
-class MedicalAppointment
+  public Dog(string name, string breed, int weight)
+  {
+    _name = name;
+    _breed = breed;
+    _weight = weight;
+  }
+
+  public Dog(string name, int weight, string breed = "mixed-breed")
+  {
+    _name = name;
+    _breed = breed;
+    _weight = weight;
+  }
+
+  public void Describe() {
+
+    string size = "Fatass";
+
+  if(_weight <= 5) {
+    size = "tiny";
+  } else if (_weight > 5 && _weight < 30)
 {
-  string _patientName;
-  DateTime _date;
-
-  public MedicalAppointment(string patientName, DateTime date)
-  {
-    _patientName = patientName;
-    _date = date;
-  }
-
-  public DateTime GetDate() => _date;
-
-  public MedicalAppointment(string patientName) : this(patientName, 7)
-  {
-  }
-
-  public MedicalAppointment(string patientName, int daysFromNow)
-  {
-    _patientName = patientName;
-    _date = DateTime.Now.AddDays(daysFromNow);
-  }
-
-  public void Reschedule(DateTime date)
-  {
-    _date = date;
-    var printer = new MedicalAppointmentPrinter();
-    printer.Print(this);
-  }
-
-
-  public void OverideMonthAndDay(int month, int day) => _date = new DateTime(_date.Year, month, day);
-
-
-  public void MoveByMonthsAndDays(int monthsToAdd, int daysToAdd)
-  {
-    _date = new DateTime(_date.Year, _date.Month + monthsToAdd, _date.Day + daysToAdd);
-  }
+  size = "medium";
 }
+else if (_weight >= 30)
+{
+  size = "large";
+  }
+        System.Console.WriteLine($"This dog is named {_name}, it's a {_breed}, and it weighs {_weight} kilograms, so it's a {size} dog.");
+  }
+    }
+
+// class MedicalAppointmentPrinter
+// {
+//   public void Print(MedicalAppointment medicalAppointment)
+//   {
+//     System.Console.WriteLine("Appointment will take place on " + medicalAppointment.GetDate());
+//   }
+// }
+
+// class MedicalAppointment
+// {
+//   string _patientName;
+//   DateTime _date;
+
+//   public MedicalAppointment(string patientName, DateTime date)
+//   {
+//     _patientName = patientName;
+//     _date = date;
+//   }
+
+//   public DateTime GetDate() => _date;
+
+//   // public MedicalAppointment(string patientName) : this(patientName, 7)
+//   // {
+//   // }
+
+//   public MedicalAppointment(string patientName, int daysFromNow = 7)
+//   {
+//     _patientName = patientName;
+//     _date = DateTime.Now.AddDays(daysFromNow);
+//   }
+
+//   public void Reschedule(DateTime date)
+//   {
+//     _date = date;
+//     var printer = new MedicalAppointmentPrinter();
+//     printer.Print(this);
+//   }
+
+
+//   public void OverideMonthAndDay(int month, int day) => _date = new DateTime(_date.Year, month, day);
+
+
+//   public void MoveByMonthsAndDays(int monthsToAdd, int daysToAdd)
+//   {
+//     _date = new DateTime(_date.Year, _date.Month + monthsToAdd, _date.Day + daysToAdd);
+//   }
+// }
 
 // var rectangle1 = new Rectangle(5,2);
 // var rectangle2 = new Rectangle(4, 9);
