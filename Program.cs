a84cd5def3cfced1a9a23b134013b0d53e624e3e
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-13 19:42:16 -0500

    understanding the "this" keyword

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-﻿
+﻿var medicalAppointment = new MedicalAppointment("Brandon", 05/30/2923);
+medicalAppointment.Reschedule(new DateTime(2023, 4, 4));
 
 Console.ReadKey();
+
+class MedicalAppointmentPrinter
+{
+  public void Print(MedicalAppointment medicalAppointment)
+  {
+    System.Console.WriteLine("Appointment will take place on " + medicalAppointment.GetDate());
+  }
+}
 
 class MedicalAppointment
 {
@@ -13,16 +22,25 @@ class MedicalAppointment
     _date = date;
   }
 
-  public MedicalAppointment(string patientName) : this(patientName, 7) {
+  public DateTime GetDate() => _date;
+
+  public MedicalAppointment(string patientName) : this(patientName, 7)
+  {
   }
 
-  public MedicalAppointment(string patientName, int daysFromNow) {
+  public MedicalAppointment(string patientName, int daysFromNow)
+  {
     _patientName = patientName;
     _date = DateTime.Now.AddDays(daysFromNow);
   }
 
-  public void Reschedule(DateTime date) => _date = date;
- 
+  public void Reschedule(DateTime date)
+  {
+    _date = date;
+    var printer = new MedicalAppointmentPrinter();
+    printer.Print(this);
+  }
+
 
   public void OverideMonthAndDay(int month, int day) => _date = new DateTime(_date.Year, month, day);
 
