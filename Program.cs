a7419c0c800f99155daad17063d1a2151b25486a
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-15 19:30:46 -0500

    understanding object initializers and the "int" keyword.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,44 @@
-﻿var taco = new Order("Banana's", new DateTime(2020, 8, 14));
-System.Console.WriteLine($"{taco.Date}");
-System.Console.WriteLine($"{taco.Item}");
+﻿// var person = new Person("John", 1981);
+var person = new Person 
+{
+  Name = "John",
+  YearOfBirth = 1981
+};
+
 
 Console.ReadLine();
 
-class Order
-    {
+class Person
+{
+  public string Name { get; set; }
+  public int YearOfBirth { get; init; }
 
-  private DateTime _date;
-  public string Item { get; set; }
-  public DateTime Date { get { return _date; } set
-    {
-      if (value.Year == DateTime.Today.Year)
-      {
-        _date = value;
-    }
-  } }
-        public Order(string item, DateTime date)
-  {
-    Item = item;
-    Date = date;
-  }
-    }
+  // public Person(string name, int yearOfBirth)
+  // {
+  //   Name = name;
+  //   YearOfBirth = yearOfBirth;
+  // }
+}
+
+
+// class Order
+//     {
+
+//   private DateTime _date;
+//   public string Item { get; set; }
+//   public DateTime Date { get { return _date; } set
+//     {
+//       if (value.Year == DateTime.Today.Year)
+//       {
+//         _date = value;
+//     }
+//   } }
+//         public Order(string item, DateTime date)
+//   {
+//     Item = item;
+//     Date = date;
+//   }
+//     }
 // var rectangle1 = new Rectangle(5,2);
 // var rectangle2 = new Rectangle(4, 9);
 
