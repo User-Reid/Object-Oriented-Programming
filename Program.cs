fc896e66014b4d1a6444b283993e24c710180334
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-15 21:29:21 -0500

    completed assignment, did it kinda first try, it was over something silly. once i had clear instruction/direction it was easy. :)

--- Program.cs
+++ Program.cs
@@ -1,64 +1,84 @@
-﻿var rectangle1 = new Rectangle(5,2);
-var rectangle2 = new Rectangle(4, 9);
+﻿// var rectangle1 = new Rectangle(5,2);
+// var rectangle2 = new Rectangle(4, 9);
 
-System.Console.WriteLine(Rectangle.DescribeGenerally());
-System.Console.WriteLine($"{rectangle1.Width}");
-System.Console.WriteLine($"{rectangle1.Height}");
-System.Console.WriteLine($"{rectangle2.Width}");
-System.Console.WriteLine($"{rectangle2.Height}");
+// System.Console.WriteLine(Rectangle.DescribeGenerally());
+// System.Console.WriteLine($"{rectangle1.Width}");
+// System.Console.WriteLine($"{rectangle1.Height}");
+// System.Console.WriteLine($"{rectangle2.Width}");
+// System.Console.WriteLine($"{rectangle2.Height}");
 
 
-System.Console.WriteLine($"1 + 2 is {Calculator.Add(1, 2)}");
-System.Console.WriteLine($"1 - 2 is {Calculator.Subtract(1, 2)}");
-System.Console.WriteLine($"1 * 2 is {Calculator.Multiply(1, 2)}");
+// System.Console.WriteLine($"1 + 2 is {Calculator.Add(1, 2)}");
+// System.Console.WriteLine($"1 - 2 is {Calculator.Subtract(1, 2)}");
+// System.Console.WriteLine($"1 * 2 is {Calculator.Multiply(1, 2)}");
+
+// Console.ReadKey();
+
+// static class Calculator
+// {
+//     public static int Add(int a, int b) => a + b;
+//     public static int Subtract(int a, int b) => a - b;
+//     public static int Multiply(int a, int b) => a * b;
+
+// }
+
+// class Rectangle
+// {
+
+//   public Rectangle(int width, int height)
+//   {
+//     Width = GetLengthOrDefault(width, nameof(Width));
+//     _height = GetLengthOrDefault(height, nameof(Height));
+//   }
+
+//   private int _height;
+//   public int Width { get; private set; }
+//   public int Height
+//   {
+//     get { return _height; }
+//     set
+//     {
+//       if (value > 0)
+//       {
+//         _height = value;
+//       }
+//     }
+//   }
+
+//   private int GetLengthOrDefault(int length, string name)
+//   {
+//     const int defaultValueIfNonPositive = 1;
+//     if (length <= 0)
+//     {
+//       System.Console.WriteLine($"{name} must be a positive number.");
+//       return defaultValueIfNonPositive;
+//     }
+//     return length;
+//   }
+
+//   public int CalculatePermimeter(Rectangle rectangle) => 2 * rectangle.Width + 2 * rectangle.Height;
+//   public int CalculateArea(Rectangle rectangle) => rectangle.Width * rectangle.Height;
+//   public string Description => $"A rectangle with width {Width} and height of {Height}";
+
+//   public static string DescribeGenerally() => $"A plane figure with four straight sides and four right angles.";
+// }
+
+var tuesday = NumberToDayOfWeekTranslator.Translate(2);
 
 Console.ReadKey();
 
-static class Calculator
+static public class NumberToDayOfWeekTranslator
 {
-    public static int Add(int a, int b) => a + b;
-    public static int Subtract(int a, int b) => a - b;
-    public static int Multiply(int a, int b) => a * b;
-
-}
-
-class Rectangle
-{
-
-  public Rectangle(int width, int height)
-  {
-    Width = GetLengthOrDefault(width, nameof(Width));
-    _height = GetLengthOrDefault(height, nameof(Height));
-  }
-
-  private int _height;
-  public int Width { get; private set; }
-  public int Height
-  {
-    get { return _height; }
-    set
+  public static string Translate(int x) =>
+    x switch
     {
-      if (value > 0)
-      {
-        _height = value;
-      }
-    }
-  }
-
-  private int GetLengthOrDefault(int length, string name)
-  {
-    const int defaultValueIfNonPositive = 1;
-    if (length <= 0)
-    {
-      System.Console.WriteLine($"{name} must be a positive number.");
-      return defaultValueIfNonPositive;
-    }
-    return length;
-  }
-
-  public int CalculatePermimeter(Rectangle rectangle) => 2 * rectangle.Width + 2 * rectangle.Height;
-  public int CalculateArea(Rectangle rectangle) => rectangle.Width * rectangle.Height;
-  public string Description => $"A rectangle with width {Width} and height of {Height}";
-
-  public static string DescribeGenerally() => $"A plane figure with four straight sides and four right angles.";
+      1 => "Monday",
+      2 => "Tuesday",
+      3 => "Wednesday",
+      4 => "Thursday",
+      5 => "Friday",
+      6 => "Saturday",
+      7 => "Sunday",
+      _ => "Invalid day of the week"
+    };
 }
