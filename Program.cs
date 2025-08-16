a3a69582bfc34396c521f31717e58bcad18034ce
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-16 17:21:07 -0500

    refactoring methods, classes and properties

--- Program.cs
+++ Program.cs
@@ -68,13 +68,86 @@
 //   public static string DescribeGenerally() => $"A plane figure with four straight sides and four right angles.";
 // }
 
-public static class StringsTransformator
+var names = new Names();
+var path = names.BuildFilePath();
+var stringsTextualRepository = new StringsTextualRepository();
+
+if (File.Exists(path))
+{
+  Console.WriteLine("Names file already exists. Loading names.");
+  var stringsFromFile = stringsTextualRepository.Read(path);
+  names.AddNames(stringsFromFile);
+}
+else
+{
+  Console.WriteLine("Names file does not yet exist.");
+
+  //let's imagine they are given by the user
+  names.AddName("John");
+  names.AddName("not a valid name");
+  names.AddName("Claire");
+  names.AddName("123 definitely not a valid name");
+
+  Console.WriteLine("Saving names to the file.");
+  stringsTextualRepository.Write(path, names.All);
+}
+Console.WriteLine(names.Format());
+
+Console.ReadLine();
+
+public class StringsTextualRepository
+{
+  private static readonly string Seperator = Environment.NewLine;
+  public List<string> Read(string filePath)
+  {
+    var fileContents = File.ReadAllText(filePath);
+    return fileContents.Split(Seperator).ToList();
+  }
+
+  public void Write(string filePath, List<string> strings) =>
+      File.WriteAllText(filePath, string.Join(Seperator, strings));
+}
+
+public class NamesValidator
+{
+  public bool IsValid(string name)
+  {
+    return
+        name.Length >= 2 &&
+        name.Length < 25 &&
+        char.IsUpper(name[0]) &&
+        name.All(char.IsLetter);
+  }
+}
+
+public class Names
+{
+  public List<string> All { get; } = new List<string>();
+  private readonly NamesValidator _namesValidator = new NamesValidator();
+
+  public void AddNames(List<string> stringsFromFile)
+  {
+    foreach (var name in stringsFromFile)
     {
-        public static string TransformSeparators(
-            string input,
-            string originalSeparator,
-            string targetSeparator)
-        {
-    return input.Replace(originalSeparator, targetSeparator);
-        }
+      AddName(name);
     }
+  }
+
+  public void AddName(string name)
+  {
+    if (_namesValidator.IsValid(name))
+    {
+      All.Add(name);
+    }
+  }
+
+  public string BuildFilePath()
+  {
+    //we could imagine this is much more complicated
+    //for example that path is provided by the user and validated
+    return "names.txt";
+  }
+
+  public string Format() =>
+      string.Join(Environment.NewLine, All);
+}
--- obj/Debug/net9.0/Object Oriented Programming.AssemblyInfo.cs
+++ obj/Debug/net9.0/Object Oriented Programming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Object Oriented Programming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e9615a987bd889a6a0ee56a5e80e1fab6dc0fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f065ff6a7fac6a88db88a588d390de581e409a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Object Oriented Programming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Object Oriented Programming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
