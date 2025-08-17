d23a043e56d57c88bd30f65b9f7faf203c74df6c
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-16 19:03:12 -0500

    understanding global usings :)

--- Program.cs
+++ Program.cs
@@ -68,30 +68,41 @@
 //   public static string DescribeGenerally() => $"A plane figure with four straight sides and four right angles.";
 // }
 using Object_Oriented_Programming.DataAccess;
+using System.Diagnostics;
 
-var names = new Names();
-var path = new NamesFilePathBuilder().BuildFilePath();
-var stringsTextualRepository = new StringsTextualRepository();
+var stopwatch = Stopwatch.StartNew();
 
-if (File.Exists(path))
+for (int i = 0; i < 1000; i++)
 {
-  Console.WriteLine("Names file already exists. Loading names.");
-  var stringsFromFile = stringsTextualRepository.Read(path);
-  names.AddNames(stringsFromFile);
+  System.Console.WriteLine($"Loop number {i}");
 }
-else
-{
-  Console.WriteLine("Names file does not yet exist.");
 
-  //let's imagine they are given by the user
-  names.AddName("John");
-  names.AddName("not a valid name");
-  names.AddName("Claire");
-  names.AddName("123 definitely not a valid name");
+stopwatch.Stop();
+System.Console.WriteLine($"Time in ms: {stopwatch.ElapsedMilliseconds}");
 
-  Console.WriteLine("Saving names to the file.");
-  stringsTextualRepository.Write(path, names.All);
-}
-Console.WriteLine(new NamesFormatter().Format(names.All));
+// var names = new Names();
+// var path = new NamesFilePathBuilder().BuildFilePath();
+// var stringsTextualRepository = new StringsTextualRepository();
 
-Console.ReadLine();
+// if (File.Exists(path))
+// {
+//   Console.WriteLine("Names file already exists. Loading names.");
+//   var stringsFromFile = stringsTextualRepository.Read(path);
+//   names.AddNames(stringsFromFile);
+// }
+// else
+// {
+//   Console.WriteLine("Names file does not yet exist.");
+
+//   //let's imagine they are given by the user
+//   names.AddName("John");
+//   names.AddName("not a valid name");
+//   names.AddName("Claire");
+//   names.AddName("123 definitely not a valid name");
+
+//   Console.WriteLine("Saving names to the file.");
+//   stringsTextualRepository.Write(path, names.All);
+// }
+// Console.WriteLine(new NamesFormatter().Format(names.All));
+
+// Console.ReadLine();
--- obj/Debug/net9.0/Object Oriented Programming.AssemblyInfo.cs
+++ obj/Debug/net9.0/Object Oriented Programming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Object Oriented Programming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe71547452bcc310f2a2dd8b21f3d0dea8f7dd17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+803e1127cc38bdfbdd39d1dca430dcc5fbbe3eef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Object Oriented Programming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Object Oriented Programming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
